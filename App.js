bd09801bb0e7bbbc086ca03691acd6bbb21dc8be
Kabilesh020799 <47252881+Kabilesh020799@users.noreply.github.com>
2020-05-08 22:56:14 +0530

    updated accelarometer sensor code

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Accelerometer } from 'expo-sensors';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Accelerometer } from "expo-sensors";
 
 export default function App() {
   const [data, setData] = useState({});
+  const [subscription, setSubscription] = useState(null);
 
   useEffect(() => {
     _toggle();
-  }, []);
 
-  useEffect(() => {
     return () => {
       _unsubscribe();
     };
   }, []);
 
   const _toggle = () => {
-    if (this._subscription) {
+    if (subscription) {
       _unsubscribe();
     } else {
       _subscribe();
@@ -32,31 +31,32 @@ export default function App() {
   };
 
   const _subscribe = () => {
-    this._subscription = Accelerometer.addListener(accelerometerData => {
+    const temp = Accelerometer.addListener((accelerometerData) => {
       setData(accelerometerData);
     });
+    setSubscription(temp);
   };
 
   const _unsubscribe = () => {
-    this._subscription && this._subscription.remove();
-    this._subscription = null;
+    subscription && subscription.remove();
+    setSubscription(null);
   };
 
   let { x, y, z } = data;
   return (
     <View style={styles.sensor}>
-      <Text style={styles.text}>Accelerometer: (in Gs where 1 G = 9.81 m s^-2)</Text>
-      <Text style={styles.text}>
+      <Text>Accelerometer: (in Gs where 1 G = 9.81 m s^-2)</Text>
+      <Text>
         x: {round(x)} y: {round(y)} z: {round(z)}
       </Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={_toggle} style={styles.button}>
+      <View>
+        <TouchableOpacity onPress={_toggle}>
           <Text>Toggle</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
+        <TouchableOpacity onPress={_slow}>
           <Text>Slow</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={_fast} style={styles.button}>
+        <TouchableOpacity onPress={_fast}>
           <Text>Fast</Text>
         </TouchableOpacity>
       </View>
@@ -71,3 +71,31 @@ function round(n) {
 
   return Math.floor(n * 100) / 100;
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    marginTop: 15,
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eee',
+    padding: 10,
+  },
+  middleButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: '#ccc',
+  },
+  sensor: {
+    marginTop: 45,
+    paddingHorizontal: 10,
+  },
+  text: {
+    textAlign: 'center',
+  },
+});
+
